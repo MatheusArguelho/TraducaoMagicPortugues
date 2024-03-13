11614f65fc71cde1827c74ab3b79520ff37b8a56
ricardo.bergamo <ricardo.bergamo@sesa.pr.gov.br>
2024-03-13 16:29:52 -0300

    commit

--- traducao_set.py
+++ traducao_set.py
@@ -50,76 +50,80 @@ def process_pages(url):
         return []
 
 
-try:
-    set_code = input("Digite o codigo do set desejado:").lower()
-    set_url = f"https://api.scryfall.com/sets/{set_code}"
-    set_json = download_json(set_url)
-    if set_json and 'search_uri' in set_json:
-        url = set_json['search_uri']
-    else:
-        raise Exception("Falha ao obter o URL de pesquisa do conjunto JSON")
-
-    all_card_data = process_pages(url)
-
-    df1 = pd.DataFrame(all_card_data)
-
-    if not df1.empty:
-        df1.index += 1
-        df1['oracle_text'] = df1['oracle_text'].fillna(value='DEBUG', inplace=False)
-
-    df1 = df1[~df1['num'].str.contains('z')]
-
-    texts = df1['oracle_text'].tolist()
-    translated_texts = []
-
-    MAX_CHAR_LIMIT = 5000
-
-    for text in tqdm(texts, desc="Translating"):
-        if isinstance(text, str) and len(text) > MAX_CHAR_LIMIT:
-            chunks = [text[i:i + MAX_CHAR_LIMIT] for i in range(0, len(text), MAX_CHAR_LIMIT)]
-            translated_texts_chunk = []
-            for chunk in chunks:
-                translated = GoogleTranslator(source='auto', target='pt').translate(text=chunk)
-                translated_texts_chunk.append(translated)
-            translated_texts.append(' '.join(translated_texts_chunk))
+def func_traducao():
+    try:
+        set_code = input("Digite o codigo do set desejado:").lower()
+        set_url = f"https://api.scryfall.com/sets/{set_code}"
+        set_json = download_json(set_url)
+        if set_json and 'search_uri' in set_json:
+            url = set_json['search_uri']
         else:
-            translated = GoogleTranslator(source='auto', target='pt').translate(text=text)
-            translated_texts.append(translated)
+            raise Exception("Falha ao obter o URL de pesquisa do conjunto JSON")
 
-    df1['traduzido'] = translated_texts
+        all_card_data = process_pages(url)
 
-    df1 = (df1.rename(columns={'num': 'numero', 'name': 'nome', 'oracle_text': 'texto_ingles'}))
+        df1 = pd.DataFrame(all_card_data)
 
-    df1.to_csv('traducao.csv', index=False)
-    # df1.to_json(f'json_{set_code}.json', orient='records')
+        if not df1.empty:
+            df1.index += 1
+            df1['oracle_text'] = df1['oracle_text'].fillna(value='DEBUG', inplace=False)
 
-    with open('traducao.csv', 'r', encoding='utf-8') as csvfile:
-        csvreader = csv.reader(csvfile)
-        rows = list(csvreader)
+        df1 = df1[~df1['num'].str.contains('z')]
 
-    html_table = '<table class="styled-table">\n'
-    for row in rows:
-        html_table += '  <tr>\n'
-        for cell in row:
-            # Replace '\n' with '<br>' in cell content
-            cell_content = cell.replace('\n', '<br>')
-            html_table += f'    <td>{cell_content}</td>\n'
-        html_table += '  </tr>\n'
-    html_table += '</table>'
+        texts = df1['oracle_text'].tolist()
+        translated_texts = []
 
-    with open("pagina_set.html", "r", encoding="utf-8") as file:
-        html_content = file.read()
+        max_char_limit = 5000
 
-    # Replace placeholders with actual values
-    html_content = html_content.format(
-        set_code=set_code,
-        html_table=html_table
-    )
+        for text in tqdm(texts, desc="Translating"):
+            if isinstance(text, str) and len(text) > max_char_limit:
+                chunks = [text[i:i + max_char_limit] for i in range(0, len(text), max_char_limit)]
+                translated_texts_chunk = []
+                for chunk in chunks:
+                    translated = GoogleTranslator(source='auto', target='pt').translate(text=chunk)
+                    translated_texts_chunk.append(translated)
+                translated_texts.append(' '.join(translated_texts_chunk))
+            else:
+                translated = GoogleTranslator(source='auto', target='pt').translate(text=text)
+                translated_texts.append(translated)
 
-    with open("traducao_set.html", "w", encoding="utf-8") as html_file:
-        html_file.write(html_content)
+        df1['traduzido'] = translated_texts
 
-    webbrowser.open("traducao_set.html")
+        df1 = (df1.rename(columns={'num': 'numero', 'name': 'nome', 'oracle_text': 'texto_ingles'}))
 
-except Exception as e:
-    print(e)
+        df1.to_csv('traducao.csv', index=False)
+        # df1.to_json(f'json_{set_code}.json', orient='records')
+
+        with open('traducao.csv', 'r', encoding='utf-8') as csvfile:
+            csvreader = csv.reader(csvfile)
+            rows = list(csvreader)
+
+        html_table = '<table class="styled-table">\n'
+        for row in rows:
+            html_table += '  <tr>\n'
+            for cell in row:
+                # Replace '\n' with '<br>' in cell content
+                cell_content = cell.replace('\n', '<br>')
+                html_table += f'    <td>{cell_content}</td>\n'
+            html_table += '  </tr>\n'
+        html_table += '</table>'
+
+        with open("pagina_set.html", "r", encoding="utf-8") as file:
+            html_content = file.read()
+
+        # Replace placeholders with actual values
+        html_content = html_content.format(
+            set_code=set_code,
+            html_table=html_table
+        )
+
+        with open("traducao_set.html", "w", encoding="utf-8") as html_file:
+            html_file.write(html_content)
+
+        webbrowser.open("traducao_set.html")
+
+    except Exception as e:
+        print(e)
+
+
+func_traducao()
