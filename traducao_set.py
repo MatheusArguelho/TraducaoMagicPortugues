a63609e2c263a20452344e2d561b12bbabda8f05
ricardo.bergamo <ricardo.bergamo@sesa.pr.gov.br>
2024-03-25 16:11:23 -0300

    commit

--- traducao_set.py
+++ traducao_set.py
@@ -6,48 +6,89 @@ from tqdm import tqdm
 import webbrowser
 import os
 
+
 def download_json(url):
     response = requests.get(url)
     if response.status_code == 200:
-        json_data = response.json()
-        return json_data
+        return response.json()
     elif response.status_code == 404:
-        print("Falha ao baixar JSON. Código de status: 404")
         raise Exception("JSON download failed due to 404 error")
     else:
-        print("Falha ao baixar JSON. Código de status:", response.status_code)
-        return None
+        raise Exception(f"JSON download failed with status code: {response.status_code}")
 
 
-def process_pages(url):
-    json_data = download_json(url)
-    if json_data:
-        if 'object' in json_data and json_data['object'] == 'error':
-            print("Error:", json_data["details"])
-            return []
-        print("JSON baixado com sucesso:")
-        card_data = []
-        for card in json_data["data"]:
-            try:
-                oracle_texto = card["oracle_text"]
-            except KeyError:
-                try:
-                    oracle_texto = card['card_faces'][0]['oracle_text'] + '\n' + '----' + '\n' + card['card_faces'][1][
-                        'oracle_text']
-                except KeyError:
-                    oracle_texto = ''
-                    print("Oracle não encontrado.")
-            card_data.append({
-                "num": card.get("collector_number", ""),
-                "name": card["name"],
-                "oracle_text": oracle_texto
-            })
-        next_page = json_data.get("next_page")
-        if next_page:
-            card_data.extend(process_pages(next_page))
-        return card_data
-    else:
-        return []
+def process_card_data(card):
+    try:
+        oracle_texto = card["oracle_text"]
+    except KeyError:
+        try:
+            oracle_texto = card['card_faces'][0]['oracle_text'] + '\n' + '----' + '\n' + card['card_faces'][1][
+                'oracle_text']
+        except KeyError:
+            oracle_texto = ''
+    return {
+        "num": card.get("collector_number", ""),
+        "name": card["name"],
+        "oracle_text": oracle_texto
+    }
+
+
+def filter_card_data(df):
+    df = df[~df['num'].str.contains('z')]
+    names_to_drop = ['Plains', 'Swamp', 'Island', 'Mountain', 'Forest']
+    return df[~df['name'].isin(names_to_drop)]
+
+
+def translate_card_texts(texts):
+    translated_texts = []
+    max_char_limit = 5000
+    for text in tqdm(texts, desc="Translating"):
+        if isinstance(text, str) and len(text) > max_char_limit:
+            chunks = [text[i:i + max_char_limit] for i in range(0, len(text), max_char_limit)]
+            translated_texts_chunk = []
+            for chunk in chunks:
+                translated = GoogleTranslator(source='auto', target='pt').translate(text=chunk)
+                translated_texts_chunk.append(translated)
+            translated_texts.append(' '.join(translated_texts_chunk))
+        else:
+            translated = GoogleTranslator(source='auto', target='pt').translate(text=text)
+            translated_texts.append(translated)
+    return translated_texts
+
+
+def save_csv_file(df, file_path):
+    df.to_csv(file_path, index=False, encoding='utf-8')
+
+
+def create_html_table(rows):
+    html_table = '<table class="styled-table">\n'
+    for row in rows:
+        html_table += '  <tr>\n'
+        for cell in row:
+            # Replace '\n' with '<br>' in cell content
+            cell_content = cell.replace('\n', '<br>')
+            html_table += f'    <td>{cell_content}</td>\n'
+        html_table += '  </tr>\n'
+    html_table += '</table>'
+    return html_table
+
+
+def read_html_template(template_path):
+    with open(template_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def fill_html_template(template_content, set_code, html_table):
+    return template_content.format(set_code=set_code, html_table=html_table)
+
+
+def save_html_file(html_content, file_path):
+    with open(file_path, "w", encoding="utf-8") as html_file:
+        html_file.write(html_content)
+
+
+def open_html_file(html_file_path):
+    webbrowser.open(html_file_path)
 
 
 def func_traducao():
@@ -62,7 +103,7 @@ def func_traducao():
         # Check if the HTML file already exists
         if os.path.exists(html_file_path):
             print(f"O arquivo {html_file_path} já existe. Abrindo o arquivo...")
-            webbrowser.open(html_file_path)
+            open_html_file(html_file_path)
             return
 
         set_url = f"https://api.scryfall.com/sets/{set_code}"
@@ -72,68 +113,41 @@ def func_traducao():
         else:
             raise Exception("Falha ao obter o URL de pesquisa do conjunto JSON")
 
-        all_card_data = process_pages(url)
+        all_card_data = []
+        while url:
+            json_data = download_json(url)
+            all_card_data.extend([process_card_data(card) for card in json_data["data"]])
+            url = json_data.get("next_page")
 
-        df1 = pd.DataFrame(all_card_data)
+        df = pd.DataFrame(all_card_data)
 
-        if not df1.empty:
-            df1.index += 1
-            df1['oracle_text'] = df1['oracle_text'].fillna(value='DEBUG', inplace=False)
+        if not df.empty:
+            df.index += 1
+            df['oracle_text'] = df['oracle_text'].fillna(value='DEBUG', inplace=False)
 
-        df1 = df1[~df1['num'].str.contains('z')]
-        names_to_drop = ['Plains', 'Swamp', 'Island', 'Mountain', 'Forest']
-        df1 = df1[~df1['name'].isin(names_to_drop)]
+        df = filter_card_data(df)
 
-        texts = df1['oracle_text'].tolist()
-        translated_texts = []
-        max_char_limit = 5000
+        translated_texts = translate_card_texts(df['oracle_text'].tolist())
 
-        for text in tqdm(texts, desc="Translating"):
-            if isinstance(text, str) and len(text) > max_char_limit:
-                chunks = [text[i:i + max_char_limit] for i in range(0, len(text), max_char_limit)]
-                translated_texts_chunk = []
-                for chunk in chunks:
-                    translated = GoogleTranslator(source='auto', target='pt').translate(text=chunk)
-                    translated_texts_chunk.append(translated)
-                translated_texts.append(' '.join(translated_texts_chunk))
-            else:
-                translated = GoogleTranslator(source='auto', target='pt').translate(text=text)
-                translated_texts.append(translated)
+        df['traduzido'] = translated_texts
 
-        df1['traduzido'] = translated_texts
+        df = df.rename(columns={'num': 'numero', 'name': 'nome', 'oracle_text': 'texto_ingles'})
 
-        df1 = (df1.rename(columns={'num': 'numero', 'name': 'nome', 'oracle_text': 'texto_ingles'}))
-
-        df1.to_csv('traducao.csv', index=False)
-        # df1.to_json(f'json_{set_code}.json', orient='records')
+        save_csv_file(df, 'traducao.csv')
 
         with open('traducao.csv', 'r', encoding='utf-8') as csvfile:
             csvreader = csv.reader(csvfile)
             rows = list(csvreader)
 
-        html_table = '<table class="styled-table">\n'
-        for row in rows:
-            html_table += '  <tr>\n'
-            for cell in row:
-                # Replace '\n' with '<br>' in cell content
-                cell_content = cell.replace('\n', '<br>')
-                html_table += f'    <td>{cell_content}</td>\n'
-            html_table += '  </tr>\n'
-        html_table += '</table>'
+        html_table = create_html_table(rows)
 
-        with open("pagina_set.html", "r", encoding="utf-8") as file:
-            html_content = file.read()
+        html_template = read_html_template("pagina_set.html")
 
-        # Replace placeholders with actual values
-        html_content = html_content.format(
-            set_code=set_code,
-            html_table=html_table
-        )
+        html_content = fill_html_template(html_template, set_code, html_table)
 
-        with open(html_file_path, "w", encoding="utf-8") as html_file:
-            html_file.write(html_content)
+        save_html_file(html_content, html_file_path)
 
-        webbrowser.open(html_file_path)
+        open_html_file(html_file_path)
 
     except Exception as e:
         print(e)
